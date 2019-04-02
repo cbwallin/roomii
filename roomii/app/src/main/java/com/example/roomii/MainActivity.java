c6db7975677a0643bbfaeb0424bee312e7961735
kmminder <49201708+kmminder@users.noreply.github.com>
2019-04-02 10:33:27 -0500

    add new tab on bottom

--- roomii/app/src/main/java/com/example/roomii/MainActivity.java
+++ roomii/app/src/main/java/com/example/roomii/MainActivity.java
@@ -26,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
+                case R.id.navigation_profile:
+                    mTextMessage.setText("Profile");
+                    return true;
             }
             return false;
         }
