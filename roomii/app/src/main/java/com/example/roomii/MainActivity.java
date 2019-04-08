6ac8e8ad242ece9df87001e0604af7468ac4cd52
Cole Wallin <cole.wallin@gmail.com>
2019-04-08 10:21:46 -0500

    Removed comments

--- roomii/app/src/main/java/com/example/roomii/MainActivity.java
+++ roomii/app/src/main/java/com/example/roomii/MainActivity.java
@@ -2,10 +2,6 @@ package com.example.roomii;
 
 import android.content.Intent;
 import android.os.Bundle;
-//import android.support.annotation.NonNull;
-//import android.support.design.widget.BottomNavigationView;
-//import android.support.v4.app.Fragment;
-//import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
