077382a95f8b5da2034d578c25dbb61d2a889f6a
Kennedy Mindermann <49201708+kmminder@users.noreply.github.com>
2019-04-14 11:11:34 -0500

    Revert "Profile edit page"

--- roomii/app/src/main/java/com/example/roomii/MainActivity.java
+++ roomii/app/src/main/java/com/example/roomii/MainActivity.java
@@ -42,6 +42,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
