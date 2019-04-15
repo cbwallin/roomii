29c8d725ec2f82fec97c3b1ae2b3d2103072f722
ZachTrosvig <trosvig@gmail.com>
2019-04-14 23:58:44 -0500

    Added settings menu in profile page and added logout functionality

--- roomii/app/src/main/java/com/example/roomii/ProfileFragment.java
+++ roomii/app/src/main/java/com/example/roomii/ProfileFragment.java
@@ -1,7 +1,11 @@
 package com.example.roomii;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -16,7 +20,29 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // return inflater.inflate(R.layout.fragment_profile, null);
         super.onCreateView(inflater, container, savedInstanceState);
-
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_profile, null);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
+        menuInflater.inflate(R.menu.menu_profile, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.edit:
+                return true;
+            case R.id.logout:
+                Intent i = new Intent(getActivity(),
+                        LoginActivity.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                        Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(i);
+                return true;
+        }
+        return false;
     }
 }
